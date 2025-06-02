312f0890dad65ca718b16735b631759522736e04
Victor <victoorres@icloud.com>
2025-06-02 19:28:37 -0300

    refactor: retirando aba de beneficios do projeto

--- app/benefits/page.tsx
+++
@@ -1,9 +0,0 @@
-import { BenefitsContent } from "@/components/benefits-content"
-
-export default function Benefits() {
-  return (
-    <div className="min-h-screen bg-[#FFEDC1] w-full pt-20">
-      <BenefitsContent />
-    </div>
-  )
-}
