f49b96e601b58498465bd4dc76d16fbec033e8dc
Victor <victoorres@icloud.com>
2025-06-02 20:11:00 -0300

    feat: adicionando aba de beneficios novamente

---
+++ app/benefits/page.tsx
@@ -0,0 +1,9 @@
+import { BenefitsContent } from "@/components/benefits-content"
+
+export default function Benefits() {
+  return (
+    <div className="min-h-screen bg-[#FFEDC1] w-full pt-20">
+      <BenefitsContent />
+    </div>
+  )
+}
