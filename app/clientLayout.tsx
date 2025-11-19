efd85fb9050a5d732575985f8aa939d45940634d
Victor <victoorres@icloud.com>
2025-11-19 00:58:40 -0300

    refactor: adicionando nova favicon

--- app/clientLayout.tsx
+++ app/clientLayout.tsx
@@ -66,11 +66,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="pt-BR">
       <head>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
-        <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
+        <meta name="apple-mobile-web-app-title" content="UP Connection" />
       </head>
       <body className={inter.className}>
         <UserProvider>
