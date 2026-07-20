c97fda9df0526b68b30d97008f4f354cbffe0dda
iamvictormt <victoorres@icloud.com>
2026-07-19 21:43:00 -0300

    Sua mensagem aqui

--- components/auth/auth-register-page.tsx
+++ components/auth/auth-register-page.tsx
@@ -5,6 +5,7 @@ import { AuthContainer } from './auth-container';
 import { RegisterFlow } from './register-flow';
 import { RegisterCarousel } from './register-carousel';
 
+
 export function AuthRegisterPage() {
   const router = useRouter();
 
